aed44471e528d31a6f2b5d5dcfc9566d464f737d
Джохар Мусаев <jomus95@gmail.com>
2021-03-24 19:31:00 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,9 +1,4 @@
 import telebot
-
-name = '';
-surname = '';
-age = 0;
-
 bot = telebot.TeleBot('1729967022:AAEfvh8IvWDe89nP38VG17Sd6MPcpiqTCCk')
 
 @bot.message_handler(commands=['start', 'help'])
@@ -16,42 +11,5 @@ def get_text_messages(message):
         bot.send_message(message.from_user.id, 'Привет!')
     else:
         bot.send_message(message.from_user.id, 'Не понимаю, что это значит.')
-
-@bot.message_handler(content_types=['text'])
-def start(message):
-    if message.text == '/reg':
-        bot.send_message(message.from_user.id, "Как тебя зовут?");
-        bot.register_next_step_handler(message, get_name); #следующий шаг – функция get_name
-    else:
-        bot.send_message(message.from_user.id, 'Напиши /reg');
-
-def get_name(message): #получаем фамилию
-    global name;
-    name = message.text;
-    bot.send_message(message.from_user.id, 'Какая у тебя фамилия?');
-    bot.register_next_step_handler(message, get_surnme);
-
-def get_surname(message):
-    global surname;
-    surname = message.text;
-    bot.send_message('В каком ты классе?');
-    bot.register_next_step_handler(message, get_age);
-
-def get_age(message):
-    global age;
-    while age == 0: #проверяем что возраст изменился
-        try:
-             age = int(message.text) #проверяем, что возраст введен корректно
-        except Exception:
-             bot.send_message(message.from_user.id, 'Цифрами, пожалуйста');
-      bot.send_message(message.from_user.id, 'Ты '+str(age)+' в классе, тебя зовут '+name+' '+surname+'?')
-    
-     keyboard = types.InlineKeyboardMarkup(); #наша клавиатура
-      key_yes = types.InlineKeyboardButton(text='Да', callback_data='yes'); #кнопка «Да»
-      keyboard.add(key_yes); #добавляем кнопку в клавиатуру
-      key_no= types.InlineKeyboardButton(text='Нет', callback_data='no');
-      keyboard.add(key_no);
-      question = 'Тебе '+str(age)+' лет, тебя зовут '+name+' '+surname+'?';
-      bot.send_message(message.from_user.id, text=question, reply_markup=keyboard)
         
 bot.polling(none_stop=True)
